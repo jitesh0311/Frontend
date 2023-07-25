3a8e16ccda99e0d1408d6c08e0517905880eb2e4
jitesh0311 <jiteshsalian1713@gmail.com>
2023-07-25 13:16:26 +0530

    Fixed Random Numbers

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCekPQKZ9VKSQyejTrE_LVX-ML06lJ7yKc",
+  authDomain: "lottery-919fb.firebaseapp.com",
+  databaseURL: "https://lottery-919fb-default-rtdb.firebaseio.com",
+  projectId: "lottery-919fb",
+  storageBucket: "lottery-919fb.appspot.com",
+  messagingSenderId: "1091879596445",
+  appId: "1:1091879596445:web:700d31587318565dc0bf74",
+  measurementId: "G-PHKF4KJC4T",
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
--- src/views/Lottery/index.jsx
+++ src/views/Lottery/index.jsx
@@ -1,67 +1,123 @@
-import React, { useState, useEffect } from 'react'
-import { Frame, FrameWrapper, LotterySect, LotteryWrapper, RandomImg, RandomImgWrapper, RollDown, RollUp, RollWrapper } from '../../styles/Lottery'
-import ResultFrame from "../../assets/frame.jpg"
-import RollUpImg from "../../assets/rollup (1).gif"
-import RollDownImg from "../../assets/rolldown.gif"
-
-
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import {
+  Frame,
+  FrameWrapper,
+  LotterySect,
+  LotteryWrapper,
+  RandomImg,
+  RandomImgWrapper,
+  RollDown,
+  RollUp,
+  RollWrapper,
+} from "../../styles/Lottery";
+import ResultFrame from "../../assets/frame.jpg";
+import RollUpImg from "../../assets/rollup (1).gif";
+import RollDownImg from "../../assets/rolldown.gif";
+import { ref, push, onValue } from "firebase/database";
+import { db } from "../../utils/firebase";
 
 const Lottery = () => {
+  const [showImage, setShowImage] = useState(true);
+  const [displayImage, setDisplayImage] = useState(false);
+  const [randomNumber1, setRandomNumber1] = useState(null);
+  const [randomNumber2, setRandomNumber2] = useState(null);
+  const [randNumber1, setRandNumber1] = useState(null);
+  const [randNumber2, setRandNumber2] = useState(null);
 
-    const [showImage, setShowImage] = useState(true);
-    const [displayImage, setDisplayImage] = useState(false)
-    const [randomNumber1, setRandomNumber1] = useState(null);
-    const [randomNumber2, setRandomNumber2] = useState(null);
+  const imageDisplayTimerRef = useRef(null);
+  const lotteryIntervalRef = useRef(null);
 
-    useEffect(() => {
-      // This effect will run once when the component mounts
-      // Set a timer to hide the image after 3-4 seconds
-      const timer = setTimeout(() => {
-        setShowImage(false);
-      }, 3000); // Change this value to 4000 for 4 seconds
-  
-      // Clean up the timer when the component unmounts
-      return () => clearTimeout(timer);
-    }, []);
+  // Wrap the function with useCallback to memoize it
+  const generateNewRandomNumbersAndDisplayImages = useCallback(() => {
+    const newRandomNumber1 = Math.floor(Math.random() * 10);
+    const newRandomNumber2 = Math.floor(Math.random() * 10);
+    setRandomNumber1(newRandomNumber1);
+    setRandomNumber2(newRandomNumber2);
 
-    const showImageAfterDelay = () => {
-        setTimeout(() => {
-          setDisplayImage(true);
-        }, 3000); // Change this value to adjust the delay (in milliseconds)
-      };
+    clearTimeout(imageDisplayTimerRef.current);
+    setShowImage(true);
+    setDisplayImage(false);
+    showImageAfterDelay();
+    push(ref(db, "numbers"), {
+      number1: newRandomNumber1,
+      number2: newRandomNumber2,
+    });
+  }, []);
 
-        const newRandomNumber1 = Math.floor(Math.random() * 10); // Generates a random number between 1 and 100
-        const newRandomNumber2 = Math.floor(Math.random() * 10);
-    
-      // Call the function to start the timer when the component mounts
-      // You can also call this function based on some user interaction (e.g., button click)
-      // For simplicity, we'll call it on component mount in this example
-      React.useEffect(() => {
-        showImageAfterDelay();
-      }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowImage(false);
+    }, 2000);
 
+    return () => clearTimeout(timer);
+  }, []);
 
+  const showImageAfterDelay = () => {
+    imageDisplayTimerRef.current = setTimeout(() => {
+      setDisplayImage(true);
+      setShowImage(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    generateNewRandomNumbersAndDisplayImages();
+
+    lotteryIntervalRef.current = setInterval(() => {
+      generateNewRandomNumbersAndDisplayImages();
+    }, 30000);
+
+    return () => {
+      clearInterval(lotteryIntervalRef.current);
+      clearTimeout(imageDisplayTimerRef.current);
+    };
+  }, [generateNewRandomNumbersAndDisplayImages]);
+
+  const fetchLatestData = useCallback(() => {
+    const numbersRef = ref(db, "numbers");
+
+    onValue(numbersRef, (snapshot) => {
+      const data = snapshot.val();
+      const entries = Object.entries(data);
+      const lastEntry = entries[entries.length - 1];
+      const lastValue = lastEntry[1];
+      const randNumber1 = lastValue.number1;
+      const randNumber2 = lastValue.number2;
+
+       setRandNumber1(randNumber1);
+      setRandNumber2(randNumber2);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchLatestData();
+  }, [fetchLatestData]);
 
   return (
     <LotterySect>
-        <LotteryWrapper>
-            <FrameWrapper>
-                <Frame src = {ResultFrame} />
-                {showImage && ( <RollWrapper>
-                    <RollUp src = {RollUpImg} />
-                    <RollDown src = {RollDownImg} />
-                </RollWrapper>
-                )}
+      <LotteryWrapper>
+        <FrameWrapper>
+          <Frame src={ResultFrame} />
+          {showImage && (
+            <RollWrapper>
+              <RollUp src={RollUpImg} />
+              <RollDown src={RollDownImg} />
+            </RollWrapper>
+          )}
 
-             {displayImage && (<RandomImgWrapper>
-                <RandomImg src = {require(`../../assets/${newRandomNumber1}.png`)} />
-                <RandomImg src = {require(`../../assets/${newRandomNumber2}.png`)} />
-                </RandomImgWrapper>
-             )}
-            </FrameWrapper>
-        </LotteryWrapper>
+          {displayImage && (
+            <RandomImgWrapper>
+              <RandomImg
+                src={require(`../../assets/${randNumber1}.png`)}
+              />
+              <RandomImg
+                src={require(`../../assets/${randNumber2}.png`)}
+              />
+            </RandomImgWrapper>
+          )}
+        </FrameWrapper>
+      </LotteryWrapper>
     </LotterySect>
-  )
-}
+  );
+};
 
-export default Lottery
+export default Lottery;
